88acaa2dc148c9803f36b1f85835cffa1f22dc9c
Nicolas Marini <nmarini44@gmail.com>
2018-06-14 22:35:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   @@all = []
-  
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all.each_char { |dog| puts dog.name }
+  end
+
+  def self.clear_all 
+    @@all.clear
+  end
+end
